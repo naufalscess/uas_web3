e45036ccf82705af1289b172628e5fa63617ed5f
naufalscess <56064794+naufalscess@users.noreply.github.com>
2020-08-26 16:04:49 +0700

    Changed
    New

---
+++ Naufal Handsome Food/resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+		<b>Version</b> 1.4.7
+	</div>
+	<strong>Copyright &copy; 2020 <a href="https://instagram.com/naufalscess" target="_blank">NAUFAL HANDSOME FOOD</a>.</strong> in Bogor, Indonesia.
+</footer>
